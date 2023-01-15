4c1b48513ac5182f1e7df51c6faba0c94c2ff0f9
Ahmed Amr <ahmed.amr@fawry.com>
2023-01-15 16:22:37 +0200

    first commit

--- AVL Sample/ViewController.swift
+++ AVL Sample/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FawryFrameworkAnonymous
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,62 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func ShowPayment(_ sender: UIButton) {
+        
+        let avlInfo = AVLInfo(length: 6 , btcWithFees: 4933, btcWithoutFees: 4428 , pans: ["512345"])
+        
+        let customerInfo = LaunchCustomerModel(customerName: "Mohamed", customerEmail: "email@gmail.com", customerMobile: "+0100000000")
+        
+        let merchantInfo = LaunchMerchantModel(merchantCode: "TUXdyCB7z5yFNpv0uxhukA==",
+                                               merchantRefNum: FrameworkHelper.shared?.getMerchantReferenceNumber(),
+                                               secureKey: "8ebd699a-2210-41fd-ae6d-e4a72cdda20a")
+        let launchModel = FawryLaunchModel(customer: customerInfo,
+                                           merchant: merchantInfo,
+                                           chargeItems: nil,
+                                           signature: nil,
+                                           avlInfo: avlInfo,
+                                           apiPath: nil)
+        
+        launchModel.skipCustomerInput = true
+        launchModel.skipReceipt = false
+        
+        launchModel.beneficiaryName = "Mohamed"
+        launchModel.beneficiaryWalletNumber = "01011100222"
+        launchModel.billingAcct = "01011100222"
+        launchModel.avlFees = 7.7
+        
+        FrameworkHelper.shared?.launchAVL(on: self,
+                                          launchModel: launchModel,
+                                          baseURL: "https://fawrydig.fawrystaging.com:2038/",
+                                          appLanguage: AppLanguage.English,
+                                          enable3Ds: true,
+                                          avlAmount: 20,
+                                          completionBlock: { (status) in
+            print("Payment Method: SDK Loaded with status \(status)")
+        }, onPreCompletionHandler: { (error) in
+            print("Payment Method: SDK Launch on pre completion \(error?.message)")
+        }, errorBlock: { (error) in
+            print(error?.errorCode)
+            print(error?.message)
+            print(error?.usedBTC)
+            print(error?.refNumber)
+            print("Payment Method: has error \(error?.message)")
+        }, onPaymentCompletedHandler: {
+            (chargeResponse) in
+            if let response = chargeResponse as? PaymentChargeResponse{
+                print("mer ref num", response.merchantRefNumber)
+                print("used BTC", response.usedBTC)
+                print("referenceNumber", response.referenceNumber)
+            }
+            
+            if let er = chargeResponse as? FawryFrameworkError{
+                print(er.message)
+            }
+        }, onSuccessHandler: { (response) in
+            let merchantRefNumber = (response as? PaymentChargeResponse)?.merchantRefNumber ?? ""
+            print("Payment Method: Success Handler: \(merchantRefNumber)")
+        })
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AVL Sample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AVL Sample
+
+pod 'FawryPaySDK-AVL'
+
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
+    end
+  end
+end
