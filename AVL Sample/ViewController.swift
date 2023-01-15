c708ac0e8b59394f2e001126c9607f7062c6ddea
Ahmed Amr <ahmed.amr@fawry.com>
2023-01-15 16:16:04 +0200

    Initial Commit

---
+++ AVL Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVL Sample
+//
+//  Created by Ahmed Sharf on 15/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
