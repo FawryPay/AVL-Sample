80cf754b5de9fe757ff411ca40faf97937b742ed
ahd wael <https://github.com/AhdWael>
2024-07-17 15:58:12 +0300

    update sample app

--- AVL Sample/ViewController.swift
+++ AVL Sample/ViewController.swift
@@ -19,36 +19,38 @@ class ViewController: UIViewController {
     
     @IBAction func ShowPayment(_ sender: UIButton) {
         
-        let avlInfo = AVLInfo(length: 6 , btcWithFees: 4933, btcWithoutFees: 4428 , pans: ["512345"], avlFeesOffUs: 7.7, avlFeesOnUs: 5.0, minValue: 10.3, maxValue: 20.3)
-        let customerInfo = LaunchCustomerModel(customerName: "Mohamed", customerEmail: "email@gmail.com", customerMobile: "+0100000000")
+        let avlInfo = AVLInfo(length: 6 , btcWithFees: 4933, btcWithoutFees: 4428 , pans: ["512345"], avlFeesOffUs: 7.7, avlFeesOnUs: 5.0)
+        
+        let customerInfo = LaunchCustomerModel(customerName: "Ahmed", customerEmail: "ahmedamrsharf@gmail.com", customerMobile: "01127927617", customerProfileId: "71917")
         
         let merchantInfo = LaunchMerchantModel(merchantCode: "siYxylRjSPyg6dz0QH/y9A==",
-                                               merchantRefNum: FrameworkHelper.shared?.getMerchantReferenceNumber(),
                                                secureKey: "086f55c1-463b-425a-9342-f75b094c8b3e")
         let launchModel = FawryLaunchModel(customer: customerInfo,
                                            merchant: merchantInfo,
-                                           chargeItems: nil,
                                            signature: nil,
+                                           paymentWithCardToken: false,
                                            avlInfo: avlInfo,
-                                           apiPath: nil)
+                                           apiPath: "ECommerceWeb/api/")
         
-        launchModel.skipCustomerInput = true
-        launchModel.skipReceipt = false
+        launchModel.skipReceipt = true
         
-//        launchModel.beneficiaryName = "Mohamed"
+     //   launchModel.beneficiaryName = "Mohamed"
         launchModel.beneficiaryWalletNumber = "01011100222"
         launchModel.billingAcct = "01010202044"
-     //   launchModel.avlFees = 7.7
+        //launchModel.avlFees = 7.7
         
         FrameworkHelper.shared?.launchAVL(on: self,
                                           launchModel: launchModel,
                                           baseURL: "https://atfawrystaging.atfawry.com/",
                                           appLanguage: AppLanguage.English,
                                           enable3Ds: true,
+                                          translationDict: nil,
                                           avlAmount: nil,
-                                          logoImage: UIImage(named: "FawryImage"),
-                                          boldFont: UIFont.boldSystemFont(ofSize: 17),
-                                          regulerFont:UIFont.italicSystemFont(ofSize: 17),
+                                          avlAmountDataType: .integer,
+                                          logoImage: nil,
+                                          titleHeader: "Fawry",
+                                          boldFont: nil,
+                                          regulerFont: UIFont(name: "SFProText-RegularItalic", size: 17),
                                           completionBlock: { (status) in
             print("Payment Method: SDK Loaded with status \(status)")
         }, onPreCompletionHandler: { (error) in
@@ -74,8 +76,79 @@ class ViewController: UIViewController {
             let merchantRefNumber = (response as? PaymentChargeResponse)?.merchantRefNumber ?? ""
             print("Payment Method: Success Handler: \(merchantRefNumber)")
             let paymentTime = (response as? PaymentChargeResponse)?.paymentTime ?? 0.0
-            let authNumber = (response as? PaymentChargeResponse)?.authNumber ?? 0
-            let cardLastFourDigits = (response as? PaymentChargeResponse)?.cardLastFourDigits ?? 0
+            let authNumber = (response as? PaymentChargeResponse)?.authNumber ?? "0"
+            let cardLastFourDigits = (response as? PaymentChargeResponse)?.cardLastFourDigits ?? "0"
+            
+            print("Payment Method: Success Handler paymentTime: \(paymentTime)")
+            print("Payment Method: Success Handler authNumber: \(authNumber)")
+            print("Payment Method: Success cardLastFourDigits: \(cardLastFourDigits)")
+        })
+    }
+    
+    @IBAction func seamlessAVLAction(_ sender: Any) {
+        
+        let customerInfo = LaunchCustomerModel(customerName: "Ahmed", customerEmail: "ahmedamrsharf@gmail.com", customerMobile: "01127927617", customerProfileId: "71917")
+        let chargeInfo = ChargeItemsParamsModel.init(itemId: "f528a3beb5274855b990dcbcc29df0d6", charge_description: "example description", price: 80.0, quantity: 1)
+        
+        let merchantInfo = LaunchMerchantModel(merchantCode: "400000011323",
+                                               secureKey: "4b815c12-891c-42ab-b8de-45bd6bd02c3d")
+        let launchModel = FawryLaunchModel(customer: customerInfo,
+                                           merchant: merchantInfo,
+                                           signature: nil,
+                                           paymentWithCardToken: false,
+                                           avlInfo: nil,
+                                           apiPath: nil,
+                                           chargeItems: [chargeInfo])
+        
+        let card = SavedCard.init(cardHolderName: "SAMEH",
+                                  cardNumber: "5123450000000008",
+                                  cardExpiryMonth: "01",
+                                  cardExpiryYear: "39",
+                                  cvv: "100")
+        
+        launchModel.skipReceipt = true
+        launchModel.beneficiaryWalletNumber = nil
+        launchModel.billingAcct = nil
+        FrameworkHelper.shared?.payUsingCreditCardSeamless(on: self,
+                                                           launchModel: launchModel,
+                                                           baseURL: "https://atfawry.fawrystaging.com/",
+                                                           appLanguage: AppLanguage.English,
+                                                           enable3Ds: true,
+                                                           translationDict: nil,
+                                                           logoImage: nil,
+                                                           titleHeader: "Fawry",
+                                                           boldFont: nil,
+                                                           regulerFont: nil,
+                                                           card: card,
+                                                           transactionPurpose: "namee",
+                                                           authCaptureModePayment: true,
+                                                           completionBlock: { (status) in
+            print("Payment Method: SDK Loaded with status \(status)")
+        }, onPreCompletionHandler: { (error) in
+            print("Payment Method: SDK Launch on pre completion \(error?.message)")
+        }, errorBlock: { (error) in
+            print(error?.errorCode)
+            print(error?.message)
+            print(error?.usedBTC)
+            print(error?.refNumber)
+            print("Payment Method: has error \(error?.message)")
+        }, onPaymentCompletedHandler: {
+            (chargeResponse) in
+            if let response = chargeResponse as? PaymentChargeResponse{
+                print("mer ref num", response.merchantRefNumber)
+                print("used BTC", response.usedBTC)
+                print("referenceNumber", response.referenceNumber)
+            }
+            
+            if let er = chargeResponse as? FawryError{
+                print(er.message)
+            }
+        }, onSuccessHandler: { (response) in
+            let merchantRefNumber = (response as? PaymentChargeResponse)?.merchantRefNumber ?? ""
+            print("Payment Method: Success Handler: \(merchantRefNumber)")
+            let paymentTime = (response as? PaymentChargeResponse)?.paymentTime ?? 0.0
+            let authNumber = (response as? PaymentChargeResponse)?.authNumber ?? "0"
+            let cardLastFourDigits = (response as? PaymentChargeResponse)?.cardLastFourDigits ?? "0"
             
             print("Payment Method: Success Handler paymentTime: \(paymentTime)")
             print("Payment Method: Success Handler authNumber: \(authNumber)")
--- Podfile
+++ Podfile
@@ -7,7 +7,7 @@ target 'AVL Sample' do
 
   # Pods for AVL Sample
 
-pod 'FawryPaySDK_AVL'
+pod 'FawryPaySDK_AVL', '0.4.0'
 
 end
 
@@ -15,7 +15,7 @@ post_install do |installer|
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
       config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'YES'
-      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '11.0'
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '12.0'
     end
   end
 end
